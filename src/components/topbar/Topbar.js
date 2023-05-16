eb356dfa216311b38c08644b97b0faf05957f29e
alihahussain <116627272+alihahussain@users.noreply.github.com>
2023-05-16 14:41:32 +0500

    Topbar.js

--- src/components/topbar/Topbar.js
+++ src/components/topbar/Topbar.js
@@ -1,11 +1,15 @@
 import "./topbar.css";
 import { Search, Person, Chat, Notifications } from "@mui/icons-material";
-
+import {Link} from "react-router-dom";
 export default function Topbar() {
+  const PF = process.env.REACT_APP_PUBLIC_FOLDER;
   return (
     <div className="topbarContainer">
       <div className="topbarLeft">
+        <Link to="/" style={{textDecoration:"none"}}>
         <span className="logo">JobHive</span>
+        </Link>
+        
       </div>
       <div className="topbarCenter">
         <div className="searchbar">
@@ -31,7 +35,7 @@ export default function Topbar() {
             <span className="topbarIconBadge">1</span>
           </div>
         </div>
-        <img src="/assets/profiles/aliha.jpeg" alt="" className="topbarImg"/>
+        <img src={`${PF}profiles/aliha.jpeg`} alt="" className="topbarImg"/>
       </div>
     </div>
   );
